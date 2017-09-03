96633793dddf8192959a6a9f80a9bcff6102b495
jay96t <31471192+jay96t@users.noreply.github.com>
2017-09-03 16:27:44 +0530

    Add files via upload

--- cd.h
+++ cd.h
@@ -1,9 +1,19 @@
+#include <cstdio>
 #ifndef CD_H
 #define CD_H
 class HD{
  public:
  HD(){}
- int cd(const char *);
+ char* cd(const char *dir){
+    char *path = NULL;
+    size_t size;
+    int rc = chdir(dir);
+    if (rc < 0) {
+        perror("cd");
+    }
+    path=getcwd(path,size);
+    return path;
+}
 };
 #endif
 
