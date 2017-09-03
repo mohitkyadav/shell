8dc5417c0057160f5d0c5a01a4e7204dc600f471
Harshendra Shah <harshendrashah812@gmail.com>
2017-09-03 19:19:41 +0530

    Update mkdir.h

--- mkdir.h
+++ mkdir.h
@@ -5,9 +5,9 @@
 using namespace std;
 
 int mkdir2(int argc,char* argv[3]) {
-        if((mkdir(argv[1],00777))==-1) {
-               cout << "Error in Creating Directory.\n";   
+    if((mkdir(argv[1],00777))==-1) {
+        cout << "Error in Creating Directory.\n";   
         }
-        else
-        		cout << argv[1] << " was created successfully.\n";
+    else
+        cout << argv[1] << " was created successfully.\n";
 }
