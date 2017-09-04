a559fba8a1d6d0a5e9542e685825b185a39263f7
RAJA BHARGAVA <raajavs@gmail.com>
2017-09-05 01:17:21 +0530

    Update ls.h

--- ls.h
+++ ls.h
@@ -1,37 +1,29 @@
-#include<dirent.h>
 #include<iostream>
+#include<dirent.h>
 
 using namespace std;
+void ls(const char*  curDir) {
 
-void ls() {
+DIR *pointerToDirectory;                                            // A type that describes a directory stream.
+struct dirent *directoryStream;                                     /* structure dirent includes the following members:
+                                                                        ino_t  d_ino       file serial number
+                                                                        char   d_name[]    name of entry                  */
 
-//DIR is a type that describes a directory stream.
-//We will create a pointer of DIR type to store the refrence of directory.
-DIR *pointerToDirectory;
 
-/* structure dirent includes the following members:
-   ino_t  d_ino       file serial number                                                                        
-   char   d_name[]    name of entry                  */
-struct dirent *directoryStream;  
-  
-/* Open the current directory.
-    / is the root of the current drive;
-   ./ is the current directory;
-   ../ is the parent of the current directory.      */
-pointerToDirectory = opendir("./");                                 
+pointerToDirectory = opendir(curDir);                                 //Open the current directory.
+                                                                    /* / is the root of the current drive;
+                                                                        ./ is the current directory;
+                                                                       ../ is the parent of the current directory. */
 
-//To check that directory is not null.
 if( pointerToDirectory != NULL) {
-    //To read the files of directory one by one and printing their names.
     while( directoryStream = readdir(pointerToDirectory)) {
         cout<<directoryStream->d_name;                              // Print the name of directories in current directory.
         cout<<"\n";
     }
-    //Closing the directory. 
     closedir(pointerToDirectory);
 }
-//If directory is null we will print an error message.
 else {
     cout<<"Couldn't open the directory";
 }
+
 }
