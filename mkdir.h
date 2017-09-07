5be0ae25ce13841cf5f64fe16e421e177fb083f0
Harshendra Shah <harshendrashah812@gmail.com>
2017-09-07 11:28:29 +0530

    Update mkdir.h

--- mkdir.h
+++ mkdir.h
@@ -1,13 +1,40 @@
-#include <stdio.h>
-#include <iostream>
-#include <sys/stat.h>
-#include <string.h>
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<dirent.h>
+#include<string.h>
+#include<unistd.h>
+
 using namespace std;
 
-int mkdir2(int argc,char* argv[3]) {
-    if((mkdir(argv[1],00777))==-1) {
-        cout << "Error in Creating Directory.\n";   
-        }
-    else
-        cout << argv[1] << " was created successfully.\n";
+int mkdir(int argc,char **argv) {
+
+	char *str, *s;
+	struct stat statBuf;
+	
+	if (argc != 2) {
+		cout << "usage:" << basename (argv[0]) << "<path>\n";
+		exit (1);
+	}
+    
+    s = argv[1];
+    
+    while ((str = strtok (s, "/")) != NULL) {
+    	if (str != s) {
+    		str[-1] = '/';
+    	}
+    	if (stat (argv[1], &statBuf) == -1) {
+    		mkdir (argv[1], 0777);
+        } 
+        else {
+        	if (! S_ISDIR (statBuf.st_mode)) {
+	        	cout << "couldn't create directory %s\n";
+	        	exit (1);
+	        }
+	    }
+        s = NULL;
+	}
+	
+	return 0; 
 }
