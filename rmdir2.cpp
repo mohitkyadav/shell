021d83a8ba78a47baff3a8c962b5875a5180e293
Abhyudai Bisht <31486107+Midnight-28@users.noreply.github.com>
2017-09-12 19:39:42 +0530

    Add rmdir2.cpp

---
+++ rmdir2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>  // used for ino_t
+#include <dirent.h>     // used for structure : struct dirent
+#include <cerrno>		// For finding error in opening directory
+#include<stdlib.h>		// exit()
+#include<cstdio>		// perror() and remove()
+#include<unistd.h>		// rmdir()
+
+
+using namespace std;
+
+DIR* d;
+int dir_finder(char*);
+int dir_finder(char* dname) // Recursive function for finding file/directory and deleteing them
+{
+ 
+    d = opendir(dname);
+    struct dirent *dir;          /* Includes :-     1) ino_t d_ino - Stores file serial number
+    			        	            2) char d_name[] - Stores name of directory  */ 													
+     if (d!=NULL)		// Returns NULL when given address is not a directory
+    {
+        while ((dir = readdir(d)) != NULL)   // On successfull completion, returns pointer to the object "dir" of type "struct dirent"
+        {
+            if (strcmp(dir->d_name, ".")==0 || strcmp(dir->d_name, "..")==0 )
+                continue;
+            cout<<"Error : Cannot delete a non-empty directory\n";
+	        return 0;
+        }
+    }
+    else
+    {
+        perror("Error : ");
+	return 0;
+    }
+}
+
